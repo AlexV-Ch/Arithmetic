1d115a807bd71ec033a815c2618fb62109e506cd
auto_1 <alexch@yandex.ru>
2024-08-08 16:53:00 +0300

    Исправлено

--- Arithmetic.java
+++ Arithmetic.java
@@ -1,53 +1,25 @@
 public class Arithmetic {
     private int pr1 = 0;
     private int pr2 = 0;
-    private int summa = 0;
-    private int proiz = 0;
-    private int max = 0;
-    private int min = 0;
 
-    public Arithmetic(int p1, int p2) {
-        pr1 = p1;
-        pr2 = p2;
+    public Arithmetic(int pr1, int pr2) {
+        this.pr1 = pr1;
+        this.pr2 = pr2;
     }
 
-    public void summa() {
-        summa = pr1 + pr2;
+    public int summa() {
+        return pr1 + pr2;
     }
 
-    public void proizv() {
-        proiz = pr1 * pr2;
+    public int proizv() {
+        return pr1 * pr2;
     }
 
-    public void max() {
-        if (pr1 > pr2) {
-            max = pr1;
-        } else if (pr2 > pr1) {
-            max = pr2;
-        } else {
-            System.out.println("Числа равны");
-            return;
-        }
-        System.out.println("Максимальное число " + max);
+    public int max() {
+        return Math.max(pr1, pr2);
     }
 
-    public void min() {
-        if (pr1 < pr2) {
-            min = pr1;
-        } else if (pr2 < pr1) {
-            min = pr2;
-        } else {
-            System.out.println("Числа равны");
-            return;
-        }
-        System.out.println("Минимальное число " + min);
+    public int min() {
+        return Math.min(pr1, pr2);
     }
-
-    public void print() {
-        System.out.println("Сумма -" + summa);
-        System.out.println("Произведение -" + proiz);
-    }
-
-
-
 }
--- Main.java
+++ Main.java
@@ -1,11 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         Arithmetic parach = new Arithmetic(8, 5);
-        parach.summa();
-        parach.proizv();
-        parach.max();
-        parach.min();
-        parach.print();
+        System.out.println("Сумма : " + parach.summa());
+        System.out.println("Произведение : " + parach.proizv());
+        System.out.println("Максимальное :" + parach.max());
+        System.out.println("Минимальное : " + parach.min());
     }
 
 }
